e664f3eb511de2267d21d6012fb0efd38d24656c
justsaykk <kaikein.woo@gmail.com>
2023-05-12 17:04:41 +0800

    Finished configuring login module

---
+++ client/src/environments/environment.default.ts
@@ -0,0 +1,12 @@
+export const defaultEnvironment = {
+    firebase: {
+      projectId: 'kenchukky-41be3',
+      appId: '1:544371851050:web:c839768e703a4c8a636853',
+      storageBucket: 'kenchukky-41be3.appspot.com',
+      apiKey: 'AIzaSyBtPt0iUQNxfjBG8UHzHAS_iPMA68Y7dCM',
+      authDomain: 'kenchukky-41be3.firebaseapp.com',
+      messagingSenderId: '544371851050',
+      measurementId: 'G-TZR4MRHZWV',
+    },
+    production: false  
+}
