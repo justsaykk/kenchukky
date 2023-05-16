1c2e2a5a3cbfef8ec3f12b0b470cf1ff11052b1c
justsaykk <kaikein.woo@gmail.com>
2023-05-16 14:35:52 +0800

    Added authentication service and email login form

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Hackathon Readme
+
+## Gihub Stuff
+
+1. When starting to work on <code>code</code>, always start on a branch. To check what branch u are on, use the command <code>git branch</code>.
+2. If you are on <code>Main</code>, use the command <code>git checkout YOUR_BRANCH_NAME</code> to switch to your desired branch.
+3. 
--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Auth } from '@angular/fire/auth';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, FirebaseuiAngularLibraryService } from 'firebaseui-angular';
 
@@ -11,13 +12,26 @@ import { FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult, Firebas
 })
 export class LoginComponent {
   private auth: Auth = inject(Auth)
+  loginForm!: FormGroup;
 
   constructor(
     private firebaseuiAngularLibraryService: FirebaseuiAngularLibraryService,
     private router: Router,
-    private location: Location 
+    private location: Location,
+    private fb: FormBuilder 
   ) {
     this.firebaseuiAngularLibraryService.firebaseUiInstance.disableAutoSignIn();
+  }
+
+  createForm() {
+    this.loginForm = this.fb.group({
+      email: this.fb.control('', [Validators.email, Validators.required]),
+      password: this.fb.control('', [Validators.required])
+    })
+  }
+
+  login() {
+    
   }
 
   // Code for firebaseAuth UI
