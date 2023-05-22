61300f9026a282e3b2aa91d1247ce4130ee90776
justsaykk <kaikein.woo@gmail.com>
2023-05-22 18:47:38 +0800

    Refactored notification code to fit in merchant UI

--- client/src/app/services/notification.service.ts
+++ client/src/app/services/notification.service.ts
@@ -3,8 +3,9 @@ import { Messaging, getMessaging, getToken, onMessage } from "firebase/messaging
 import { FirebaseAuthenticationService } from './firebase-authentication.service';
 import { environment } from 'src/environments/environment';
 import { User } from '@angular/fire/auth';
-import { BehaviorSubject, Subscription } from 'rxjs';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
 import { BackendService } from './backend.service';
+import { NotificationData, Order } from '../models/models';
 
 @Injectable({
   providedIn: 'root'
@@ -14,22 +15,41 @@ export class NotificationService implements OnDestroy{
   messaging!: Messaging
   user!: User | null
   user$!: Subscription
-  private message = new BehaviorSubject<string>("");
-
+  private _message = new BehaviorSubject<string>("");
+  private _notificationData = new BehaviorSubject<NotificationData>({
+    senderToken: null,
+    orderId: null,
+    customerName: null,
+    qty: null,
+    uom: null
+  });
 
   constructor(
     private authSvc: FirebaseAuthenticationService,
     private backendSvc: BackendService,
   ) { 
     this.messaging = getMessaging()
-    // Handle foreground notification
     onMessage(this.messaging, (payload) => {
-      let body = payload.notification?.body;
-      body ? this.message.next(body) : this.message.next("");
+      let body = payload.notification?.body!;
+      if (payload.data) {
+        let data = payload.data;
+        let notificationData: NotificationData = {
+          senderToken: data['senderToken'],
+          orderId: data["orderId"],
+          customerName: data["customerName"],
+          qty: parseInt(data["qty"]),
+          uom: data["uom"]
+        }
+        this._notificationData.next(notificationData)
       }
+      this._message.next(body);
+    }
     );
     this.user$ = this.authSvc.user$.subscribe((user) => this.user = user)
   }
+
+  public getMessage(): Observable<string> {return this._message.asObservable()};
+  public getnotificationData(): Observable<NotificationData> {return this._notificationData.asObservable()};
 
   async fbGenerateToken() {
     const currentToken = await getToken(this.messaging, { vapidKey: environment.firebase.vapidKey})
